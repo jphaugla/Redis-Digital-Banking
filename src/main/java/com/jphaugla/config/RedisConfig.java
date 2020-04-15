aabcdedb7dc96585e95212f915727f910b36609e
Jason Haugland <jasonhaugland@Jasons-MacBook-Pro-2.local>
2020-04-15 16:03:26 -0500

    fix config to allow host change

--- src/main/java/com/jphaugla/config/RedisConfig.java
+++ src/main/java/com/jphaugla/config/RedisConfig.java
@@ -2,9 +2,12 @@ package com.jphaugla.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-
+@Configuration
+@ComponentScan("com.jphaugla")
 public class RedisConfig {
     @Value("${redis_host:localhost}")
     private String redis_host;
