fb3fe78de2af1310e68e5dec409b8e7b7e584f58
jphaugla <jasonhaugland@gmail.com>
2020-04-17 04:22:12 +0000

    tweak the scripts for better performance

--- scripts/startAppServers.sh
+++ scripts/startAppServers.sh
@@ -2,3 +2,5 @@ nohup java -DServer.port=8080 -jar ../target/redis-0.0.1-SNAPSHOT.jar > /tmp/app
 nohup java -DServer.port=8081 -jar ../target/redis-0.0.1-SNAPSHOT.jar > /tmp/appServer8081.out 2>&1 &
 nohup java -DServer.port=8082 -jar ../target/redis-0.0.1-SNAPSHOT.jar > /tmp/appServer8082.out 2>&1 &
 nohup java -DServer.port=8083 -jar ../target/redis-0.0.1-SNAPSHOT.jar > /tmp/appServer8083.out 2>&1 &
+nohup java -DServer.port=8084 -jar ../target/redis-0.0.1-SNAPSHOT.jar > /tmp/appServer8084.out 2>&1 &
+nohup java -DServer.port=8085 -jar ../target/redis-0.0.1-SNAPSHOT.jar > /tmp/appServer8085.out 2>&1 &
